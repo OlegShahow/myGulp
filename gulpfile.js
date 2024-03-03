04dc0e2d4792c40e8a54b432da60d4c08407db4a
Oleg Shahow <shahoww38@gmail.com>
2024-03-03 18:09:11 +0200

    +html css js таски

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@
 
 const { src, dest } = require("gulp");
 const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
+// const autoprefixer = require("gulp-autoprefixer");
 const cssbeautify = require("gulp-cssbeautify");
-const removeComments = require("gulp-strip-css-comments");
+// const removeComments = require("gulp-strip-css-comments");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
@@ -14,6 +14,7 @@ const panini = require("panini");
 const imagemin = require("gulp-imagemin");
 const del = require("del");
 const browserSync = require("browser-sync").create();
+// const rigger = require("gulp-rigger");
 
 // пути
 const srcPath = "src/";
@@ -47,3 +48,51 @@ const path = {
   },
   clean: "./" + distPath,
 };
+
+// ---------------------------  task --------------------------------------------
+
+function html() {
+  return src(path.src.html, { base: srcPath })
+    .pipe(plumber())
+    .pipe(dest(path.build.html));
+}
+
+function css() {
+  return src(path.src.css, { base: srcPath + "assets/scss/" })
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(cssbeautify())
+    .pipe(dest(path.build.css))
+    .pipe(
+      cssnano({
+        zindex: false,
+        discardComments: {
+          removeAll: true,
+        },
+      })
+    )
+    .pipe(
+      rename({
+        suffix : ".min",
+        extname: ".css",
+      })
+    )
+    .pipe(dest(path.build.css));
+}
+
+function js(){
+    return src(path.src.js, { base: srcPath + "assets/js/" })
+    .pipe(plumber())
+    // .pipe(rigger())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(rename({
+        suffix : ".min",
+        extname:".js"
+    }))
+    .pipe(dest(path.build.js));
+}
+
+exports.html = html;
+exports.css = css;
+exports.js = js;
