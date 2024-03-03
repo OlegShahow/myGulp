a5246e91d6d911ad05b96913d61622c431b66adb
Oleg Shahow <shahoww38@gmail.com>
2024-03-03 16:44:00 +0200

    + пути

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const autoprefixer = require("gulp-autoprefixer");
 const cssbeautify = require("gulp-cssbeautify");
 const removeComments = require("gulp-strip-css-comments");
 const rename = require("gulp-rename");
-const sass = require("gulp-sass")(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
 const uglify = require("gulp-uglify");
 const plumber = require("gulp-plumber");
@@ -14,3 +14,36 @@ const panini = require("panini");
 const imagemin = require("gulp-imagemin");
 const del = require("del");
 const browserSync = require("browser-sync").create();
+
+// пути
+const srcPath = "src/";
+const distPath = "dist/";
+
+const path = {
+  build: {
+    html: distPath,
+    css: distPath + "assets/css/",
+    js: distPath + "assets/js",
+    images: distPath + "assets/images/",
+    fonts: distPath + "assets/fonts/",
+  },
+  src: {
+    html: srcPath + "*.html",
+    css: srcPath + "assets/scss/*.scss",
+    js: srcPath + "assets/js/*.js",
+    images:
+      srcPath +
+      "assets/images/**/*.{jpg,png,svg,gif,ico,webp,xml,json,webmanifest}",
+    fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+  },
+  watch: {
+    html: srcPath + "**/*.html",
+    css: srcPath + "assets/scss/**/*.scss",
+    js: srcPath + "assets/js/**/*.js",
+    images:
+      srcPath +
+      "assets/images/**/*.{jpg,png,svg,gif,ico,webp,xml,json,webmanifest}",
+    fonts: srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}",
+  },
+  clean: "./" + distPath,
+};
